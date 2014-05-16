7ad49bcf079c04ed62e8691b2c8466b9bcb4ae72
HybridRbt <littlejack0120@gmail.com>
2014-05-16 11:00:47 -0500

    overcomplicated
    simply convert string to float to get rid of the spaces before the
    numbers

---
+++ assn6-5.py
@@ -0,0 +1,22 @@
+__author__ = 'jeredyang'
+
+# 6.5 Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
+# Convert the extracted value to a floating point number and print it out.
+
+text = "X-DSPAM-Confidence:    0.8475"
+
+
+# def revert_string(string):
+#     result = ""
+#     for index in range(len(string) - 1, -1, -1):
+#         result += string[index]
+#
+#     return result
+#
+#
+# rev_text = revert_string(text)
+# sliced_text = rev_text[:rev_text.find(" ")]
+#
+# print float(revert_string(sliced_text))
+
+
